38a653f9baa8ff63ef344f32bbd2f0f24386676f
A.C.Dilshan <101160440+chamithgithub@users.noreply.github.com>
2024-07-18 12:42:33 +0530

    css up

--- simplepizzaapp/src/components/Navbar.jsx
+++ simplepizzaapp/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import './Navbar.css';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav>
-       <Link className="title" to="/">Home</Link>
+       <NavLink className="title" to="/">Home</NavLink>
       <ul>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/login">Log In</Link></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+        <li><NavLink to="/login">Log In</NavLink></li>
       </ul>
     </nav>
   );
